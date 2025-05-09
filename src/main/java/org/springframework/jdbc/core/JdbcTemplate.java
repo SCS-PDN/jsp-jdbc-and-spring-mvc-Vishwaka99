f387dabce145bea8688de4a175e5594c54432903
Vishwaka99 <166687279+Vishwaka99@users.noreply.github.com>
2025-05-09 23:05:45 +0530

    Update JdbcTemplate.java

--- src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -6,12 +6,12 @@ import java.util.Map;
 public class JdbcTemplate {
 
 	public int queryForObject(String sql, Object[] objects, Class<Integer> class1) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	public List<Map<String, Object>> queryForList(String sql) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
